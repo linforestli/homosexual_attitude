080d302fcca28c89d30aeeb6bee1baad7deda087
Linrong Li <lilinrong60@gmail.com>
2023-04-03 15:39:46 -0400

    Update on data cleaning

--- scripts/02-data_cleaning.R
+++ scripts/02-data_cleaning.R
@@ -1,26 +1,34 @@
 #### Preamble ####
-# Purpose: Cleans.... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Data: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Reads and cleans the data set needed for the analysis
+# Author: Linrong Li
+# Data: 3 April 2023
+# Contact: linrongl.li@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: None
 
 
 #### Workspace setup ####
 library(tidyverse)
-# [...UPDATE THIS...]
+library(readxl)
+library(here)
 
-#### Clean data ####
-# [...UPDATE THIS...]
-raw_data <- read_csv("inputs/data/raw_data.csv")
+#### Read data ####
+raw_data <- read_excel(here::here("inputs/data/GSS.xlsx"))
 
+#### Remove all N/As ####
+cleaned_teach <- raw_data %>%
+  select(id_, occ10, age, educ, sex, colhomo, class_, prestg10)
 
+for (i in seq_along(cleaned_teach)) {
+  # Use grepl to find cells starting with ".s", ".n.", ".i", or ".d"
+  bad_cells <- grepl("^\\.s|^\\.n|^\\.i|^\\.d", cleaned_teach[[i]])
+  # Replace bad cells with NA
+  cleaned_teach[bad_cells, i] <- NA
+}
 
+cleaned_teach <- drop_na(cleaned_teach)
 
 #### Save data ####
 # [...UPDATE THIS...]
 # change cleaned_data to whatever name you end up with at the end of cleaning
-write_csv(cleaned_data, "outputs/data/cleaned_data.csv")
 
